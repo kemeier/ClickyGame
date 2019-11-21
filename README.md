29b64fc9c6d1483a0bd4c0d7b3fb42c8f0aab154
Kelly Meier <kemeier2000@yahoo.com>
2019-11-21 11:38:26 -0600

    fixed display of cards and added read me

---
+++ README.md
@@ -0,0 +1,7 @@
+Clicky Game is a React application that uses Create React App.
+It renders different images to the screen, and each image listens for click events.
+
+The application also keeps track of the user's score, which is incremented when clicking an image for the first time. The user's score resets to 0 if they click the same image more than once.
+
+Every time the user clicks an image, the images rendered to the page shuffle themselves in random order. Once the user's score is reset after an incorrect guess, the game restarts.
+
--- src/App.js
+++ src/App.js
@@ -15,14 +15,16 @@ class App extends Component {
       <Wrapper>
         <Title>Memory Game: Tribute to the Chihuahua</Title>
         <h1>Click on a Chihuahua to earn points, but don't click any Chihuahua more than once!!</h1>
+          {this.state.cards.map(card => (
           <Cards
-            id={cards.id}
-            key={cards.id}
-            name={cards.name}
-            image={cards.image}
-            occupation={cards.occupation}
-            location={cards.location}
+            id={card.id}
+            key={card.id}
+            name={card.name}
+            image={card.image}
+            occupation={card.occupation}
+            location={card.location}
           />
+          ))}
       </Wrapper>
     );
   }
