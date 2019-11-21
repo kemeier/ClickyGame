0ff53c17a7a1eef16a7c7f348f87b804e694dab7
Kelly Meier <kemeier2000@yahoo.com>
2019-11-21 10:41:44 -0600

    additional fixes

---
+++ build/precache-manifest.f2a2b99e5e88a97b806256192436e914.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe46f347420ac5fc2c5b1a1ba1a459f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfdf0fa33a84b79b93ef",
+    "url": "/static/css/main.573884c4.chunk.css"
+  },
+  {
+    "revision": "4fb6c9bea8f558224c16",
+    "url": "/static/js/2.b7a6145f.chunk.js"
+  },
+  {
+    "revision": "cfdf0fa33a84b79b93ef",
+    "url": "/static/js/main.63177fbd.chunk.js"
+  },
+  {
+    "revision": "74eeb5effd5df1c011e2",
+    "url": "/static/js/runtime-main.6872df7b.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
 import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
+import Cards from "./components/Cards";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
-import friends from "./friends.json";
+import cards from "./cards.json";
 
 class App extends Component {
 
   state = {
-    friends
+    cards
   };
-
-  removeFriend = id => {
-   
-    const friends = this.state.friends.filter(friend => friend.id !== id);
-  
-    this.setState({ friends });
-  };
-
 
   render() {
     return (
       <Wrapper>
         <Title>Memory Game: Tribute to the Chihuahua</Title>
         <h1>Click on a Chihuahua to earn points, but don't click any Chihuahua more than once!!</h1>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            removeFriend={this.removeFriend}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+          <Cards
+            id={cards.id}
+            key={cards.id}
+            name={cards.name}
+            image={cards.image}
+            occupation={cards.occupation}
+            location={cards.location}
           />
-        ))}
       </Wrapper>
     );
   }
